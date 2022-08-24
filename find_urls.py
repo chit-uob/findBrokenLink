fe9e1a02a488144de443479382590b219466b3ad
User <sample@email.com>
2022-08-24 22:51:15 +0100

    urlextract version

--- find_urls.py
+++ find_urls.py
@@ -1,9 +1,10 @@
-import re
+from urlextract import URLExtract
 
 with open('data/CS100 Topic 1 Study Guide Summer Term 2022-23-final.txt', 'r', encoding='utf-8') as f:
     s = f.read()
 
 # print(s)
 
-urls = re.findall("(https?://\S+)", s)
+extractor = URLExtract()
+urls = extractor.find_urls(s)
 print(', '.join(urls))
